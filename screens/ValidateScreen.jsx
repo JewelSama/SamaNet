f6fd50063ef133c35796a14eea193019c3acc28b
Jewel Osafile <osafilejewel@gmail.com>
2023-07-01 20:09:27 +0100

    ValidateScr

---
+++ screens/ValidateScreen.jsx
@@ -0,0 +1,15 @@
+import { SafeAreaView, View, Text } from 'react-native'
+import React from 'react'
+import GlobalStyles from '../Config/GlobalStyles'
+
+const ValidateScreen = () => {
+  return (
+    <SafeAreaView style={GlobalStyles.droidSafeArea} className="h-full bg-white">
+      <View className="px-7">
+
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default ValidateScreen
