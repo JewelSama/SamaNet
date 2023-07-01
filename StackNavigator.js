ecaddc12c44bfe8e1db6a6067d13e97bfaff0b29
Jewel Osafile <osafilejewel@gmail.com>
2023-07-01 20:08:56 +0100

    Validate screen

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,6 +3,7 @@ import HomeScreen from "./screens/HomeScreen"
 import SignInScreen from "./screens/SignInScreen"
 import SignUpScreen from "./screens/SignUpScreen"
 import Welcome from "./screens/Welcome"
+import ValidateScreen from "./screens/ValidateScreen"
 
 const StackNavigator = () => {
     const Stack = createStackNavigator()
@@ -11,9 +12,10 @@ const StackNavigator = () => {
       headerShown: false
     }}>
         <Stack.Screen name="Welcome" component={Welcome} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="SignIn" component={SignInScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Validate" component={ValidateScreen} />
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   )
 }
