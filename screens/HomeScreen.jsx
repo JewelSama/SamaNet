541c1ffbfa69aae0efd1aa1f4dfcfe7ed7b8ff5a
Jewel Osafile <osafilejewel@gmail.com>
2023-07-04 22:16:29 +0100

    home scr

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -4,6 +4,9 @@ import GlobalStyles from '../Config/GlobalStyles'
 import { Ionicons, Entypo } from "@expo/vector-icons"
 import Logo from "../assets/icon1.png"
 import Pfp from "../assets/avatar3.jpg"
+import Post1 from "../assets/post1.jpeg"
+import Post3 from "../assets/post3.jpeg"
+import Post4 from "../assets/post4.jpeg"
 
 
 const HomeScreen = ({ navigation }) => {
@@ -16,10 +19,10 @@ const HomeScreen = ({ navigation }) => {
           <View className="flex-1"><Text className="text-3xl font-bold text-slate-700">SamaNet</Text></View>
           <View className="flex flex-row space-x-2">
 
-            <TouchableOpacity className="bg-gray-300 p-2 rounded-full">
+            <TouchableOpacity onPress={() => navigation.navigate('Search')} className="bg-gray-300 p-2 rounded-full">
             <Entypo name="magnifying-glass" size={24} color="rgb(51, 65, 85)" />
             </TouchableOpacity>
-            <TouchableOpacity className="bg-gray-300 p-2 rounded-full">
+            <TouchableOpacity onPress={() => navigation.navigate('Profile')} className="bg-gray-300 p-2 rounded-full">
               <Ionicons name="person" size={24} color="rgb(51, 65, 85)" />
             </TouchableOpacity>
             <TouchableOpacity onPress={() => navigation.navigate('ChatList')} className="bg-gray-300 p-2 rounded-full">
@@ -46,13 +49,13 @@ const HomeScreen = ({ navigation }) => {
             />
           </View>
           <View className="items-center">
-            <Text className="text-md font-bold text-slate-700 mt-3">@Username</Text>
+            <Text className="text-md font-bold text-white mt-3">@Username</Text>
           </View>
         </TouchableOpacity>
 
         <TouchableOpacity className=" h-44 w-32 bg-red-300 rounded-lg">
           <Image 
-            source={Logo}
+            source={Post4}
             className="h-full  w-full rounded-lg"
           />
           <View className="self-center absolute left-1 top-1 rounded-full h-8 w-8 items-center justify-center bg-[#eeca70]">
@@ -62,13 +65,13 @@ const HomeScreen = ({ navigation }) => {
             />
           </View>
           <View className="items-center absolute bottom-3 left-0 right-0">
-            <Text className="text-md font-bold text-slate-700 mt-3">@Username</Text>
+            <Text className="text-md font-bold text-white mt-3">@JewelSama</Text>
           </View>
         </TouchableOpacity>
 
         <TouchableOpacity className=" h-44 w-32 bg-red-300 rounded-lg">
           <Image 
-            source={Logo}
+            source={Post3}
             className="h-full  w-full rounded-lg"
           />
           <View className="self-center absolute left-1 top-1 rounded-full h-8 w-8 items-center justify-center bg-[#eeca70]">
@@ -78,13 +81,13 @@ const HomeScreen = ({ navigation }) => {
             />
           </View>
           <View className="items-center absolute bottom-3 left-0 right-0">
-            <Text className="text-md font-bold text-slate-700 mt-3">@Username</Text>
+            <Text className="text-md font-bold text-white mt-3">@Username</Text>
           </View>
         </TouchableOpacity>
 
         <TouchableOpacity className=" h-44 w-32 bg-red-300 rounded-lg">
           <Image 
-            source={Logo}
+            source={Post1}
             className="h-full  w-full rounded-lg"
           />
           <View className="self-center absolute left-1 top-1 rounded-full h-8 w-8 items-center justify-center bg-[#eeca70]">
@@ -94,14 +97,14 @@ const HomeScreen = ({ navigation }) => {
             />
           </View>
           <View className="items-center absolute bottom-3 left-0 right-0">
-            <Text className="text-md font-bold text-slate-700 mt-3">@Username</Text>
+            <Text className="text-md font-bold text-white mt-3">@Username</Text>
           </View>
         </TouchableOpacity>
 
       </ScrollView>
-      <TouchableOpacity className="bg-gray-400 p-2 w-10 h-10 self-center mt-2 rounded-full">
+      <TouchableOpacity className="bg-gray-300 p-2 w-10 h-10 self-center mt-2 rounded-full" onPress={() => navigation.navigate('Create')}>
             <Entypo name="plus" size={24} color="rgb(51, 65, 85)" />
-            </TouchableOpacity>
+      </TouchableOpacity>
 
       <View className="flex flex-row h-14 w-full mt-4 rounded-full bg-gray-200 ">
         <Pressable onPress={() => setField(0)} className={`rounded-full h-full w-1/2  items-center justify-center ${feed === 0 &&  'bg-[#eeca70]'}`}>
@@ -120,7 +123,9 @@ const HomeScreen = ({ navigation }) => {
      
     
   {feed === 1 && (
-  <Text>Discover</Text>  
+    <View className="items-center justify-center mt-32">
+      <Text className="text-3xl font-semibold text-gray-300">Coming Soon</Text>
+    </View>
   )}
 
       </View>
