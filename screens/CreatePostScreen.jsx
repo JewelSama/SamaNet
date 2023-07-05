f259634eb29257cceb962fe8b3a6f738d0aff0b4
Jewel Osafile <osafilejewel@gmail.com>
2023-07-05 16:57:49 +0100

    create post scr

--- screens/CreatePostScreen.jsx
+++ screens/CreatePostScreen.jsx
@@ -1,26 +1,65 @@
-import { View, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native'
-import { Entypo } from '@expo/vector-icons'
+import { View, Text, SafeAreaView, TouchableOpacity, Image, ScrollView, TextInput } from 'react-native'
+import { Entypo, MaterialIcons } from '@expo/vector-icons'
 import Logo from "../assets/logo.png"
 
 
 
 
 const CreatePostScreen = ({ navigation }) => {
+  const Post = () => {
+
+  }
+
+
   return (
     <SafeAreaView className="bg-gray-100 h-full">
         <View className="px-4">
             <View className="flex flex-row">
-                <Text className="flex-1 text-3xl font-bold text-slate-700">Create Post</Text>
-                <TouchableOpacity className="h-10 w-10 rounded-full bg-gray-200 items-center justify-center" onPress={() => navigation.goBack()}>
+                <TouchableOpacity className=" h-10 w-10 rounded-full bg-gray-200 items-center justify-center" onPress={() => navigation.goBack()}>
                     <Entypo name='chevron-left' size={34} color="rgb(51, 65, 85)" />
                 </TouchableOpacity>
+                <Text className=" text-3xl font-bold text-slate-700"></Text>
             </View>
-            <View className="w-full items-left mt-5">
+            <View className="w-full items-center mt-1">
               <Image 
                 source={Logo}
                 className="h-10 w-10"
                 />
             </View>
+            <>
+              <ScrollView className="h-full mt-2" showsVerticalScrollIndicator={false} automaticallyAdjustKeyboardInsets={true}  contentContainerStyle={{paddingBottom: 60}}>
+                
+              <View className="space-y-1">
+                <Text className="text-xl mr-1 font-bold">Caption</Text>
+                <TextInput
+                  className=" px-3 text-lg border border-gray-200 py-1 pb-2 font-semibold w-full max-h-64 rounded-md"
+                  placeholder='Write something...'
+                  textAlignVertical='center'
+                  multiline={true}
+                  autoCapitalize='words'
+                />
+              </View>
+
+              <TouchableOpacity className="flex flex-row justify-center h-48 space-x-2 mt-16 bg-gray-200  items-center rounded-md">
+                <MaterialIcons name='photo-library' size={34} color="rgb(51, 65, 85)" />
+                <Text className="font-bold text-lg text-slate-700">Select Photo</Text>
+              </TouchableOpacity>
+
+              {/* <View className="mt-4">
+                <Image 
+                  source={Logo}
+                  className="h-32"
+                />
+              </View> */}
+
+            <TouchableOpacity
+              className="bg-[#eeca70] h-16 w-full items-center mt-20  justify-center rounded-md" 
+              onPress={Post}>
+                <Text className="font-bold text-white text-xl">Post</Text>
+            </TouchableOpacity>
+
+              </ScrollView>
+            </>
         </View>
     </SafeAreaView>    
   )
