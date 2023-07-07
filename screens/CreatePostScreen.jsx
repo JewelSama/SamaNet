2f0fde14e3531231ec28dc39a8f7ebf39f830bd4
Jewel Osafile <osafilejewel@gmail.com>
2023-07-07 13:15:00 +0100

    image picker func

--- screens/CreatePostScreen.jsx
+++ screens/CreatePostScreen.jsx
@@ -1,11 +1,36 @@
 import { View, Text, SafeAreaView, TouchableOpacity, Image, ScrollView, TextInput } from 'react-native'
 import { Entypo, MaterialIcons } from '@expo/vector-icons'
 import Logo from "../assets/logo.png"
+import { useState } from "react"
+import * as ImagePicker from "expo-image-picker"
 
 
 
 
 const CreatePostScreen = ({ navigation }) => {
+  const [ image, setImage ] = useState(null)
+
+  const PickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      // allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+      selectionLimit: 1
+    }) 
+    console.log(result)
+
+    if(!result.canceled){
+      setImage(result.assets[0].uri)
+    }
+    console.log(image)
+  }
+
+
+
+
+
   const Post = () => {
 
   }
@@ -40,20 +65,25 @@ const CreatePostScreen = ({ navigation }) => {
                 />
               </View>
 
-              <TouchableOpacity className="flex flex-row justify-center h-48 space-x-2 mt-16 bg-gray-200  items-center rounded-md">
+              {!image ? <TouchableOpacity className="flex flex-row justify-center h-48 space-x-2 mt-16 bg-gray-200  items-center rounded-md" onPress={PickImage}>
                 <MaterialIcons name='photo-library' size={34} color="rgb(51, 65, 85)" />
                 <Text className="font-bold text-lg text-slate-700">Select Photo</Text>
               </TouchableOpacity>
-
-              {/* <View className="mt-4">
-                <Image 
-                  source={Logo}
-                  className="h-32"
-                />
-              </View> */}
+                : (
+                    <>
+                      <View className="mt-5 rounded-sm">
+                        <Image 
+                          source={{ uri: image }}
+                          className="h-96"
+                        />
+                      </View>
+                      <TouchableOpacity className="items-center mt-4 bg-gray-200 p-2 rounded-md" onPress={PickImage}><Text className="text-lg font-bold text-slate-700">Change Image</Text></TouchableOpacity>
+                    </>
+                )
+              }
 
             <TouchableOpacity
-              className="bg-[#eeca70] h-16 w-full items-center mt-20  justify-center rounded-md" 
+              className="bg-[#eeca70] h-16 w-full items-center mt-14  justify-center rounded-md" 
               onPress={Post}>
                 <Text className="font-bold text-white text-xl">Post</Text>
             </TouchableOpacity>
