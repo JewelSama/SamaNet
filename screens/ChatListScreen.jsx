b4351a73c7b8b02845c676e0a29a7020f6dac800
Jewel Osafile <osafilejewel@gmail.com>
2023-07-10 15:42:45 +0100

    chatlist scr

--- screens/ChatListScreen.jsx
+++ screens/ChatListScreen.jsx
@@ -1,12 +1,27 @@
+import {SafeAreaView, View, Text, TouchableOpacity, ScrollView } from 'react-native'
+import { Entypo } from "@expo/vector-icons"
+import Chat from '../components/Chat'
 
-import { View, Text } from 'react-native'
-import React from 'react'
-
-const ChatListScreen = () => {
+const ChatListScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>ChatListScreen</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+        <View className="px-4">
+            <View className="flex flex-row">
+                <Text className="flex-1 text-3xl font-bold text-slate-700">Chat</Text>
+                <TouchableOpacity className="h-10 w-10 rounded-full bg-gray-300 items-center justify-center" onPress={() => navigation.goBack()}>
+                    <Entypo name='chevron-left' size={34} color="rgb(51, 65, 85)" />
+                </TouchableOpacity>
+            </View>
+          <>
+            <ScrollView className="mt-6" showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 20}}>
+              <Chat />
+              <Chat />
+              <Chat />
+            </ScrollView>
+          </>
+
+          </View>
+     </SafeAreaView>       
   )
 }
 
